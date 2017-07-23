c7c1b7b37a48cc83b7eb5d0ea261ef2fe5ec1756
Melissa Yu <melissa.y00@gmail.com>
2017-07-22 17:34:41 -0700

    first commit

---
+++ app/src/main/java/com/myu/android/housemate/MainActivity.java
@@ -0,0 +1,13 @@
+package com.myu.android.housemate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
